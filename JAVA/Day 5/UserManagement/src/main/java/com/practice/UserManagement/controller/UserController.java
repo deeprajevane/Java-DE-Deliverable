11f542ace15c5d8844de433c461ee934c6458031
deeprajevane <deepraj.evane@nucleusteq.com>
2025-05-16 11:17:23 +0530

    Global Exception Handling Added

--- JAVA/Day 5/UserManagement/src/main/java/com/practice/UserManagement/controller/UserController.java
+++ JAVA/Day 5/UserManagement/src/main/java/com/practice/UserManagement/controller/UserController.java
@@ -3,10 +3,13 @@ package com.practice.UserManagement.controller;
 
 import com.practice.UserManagement.dto.UserRequestDto;
 import com.practice.UserManagement.dto.UserResponseDto;
+import com.practice.UserManagement.exception.BadRequestException;
+import com.practice.UserManagement.exception.UserNotFoundException;
 import com.practice.UserManagement.model.User;
 import com.practice.UserManagement.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +27,11 @@ public class UserController {
     public ResponseEntity<String> runDemo() {
         userService.createUserWithAddressAndCompany();
         return ResponseEntity.ok("Transaction completed");
+
+    }
+    @GetMapping("/users/{email}")
+    public User getUser(@PathVariable String email) {
+        return userService.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found with email: " + email));
     }
 }
